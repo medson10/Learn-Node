5de392dc97c63d9ecac3fe4287f77af3ed76ddc1
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-01-04 00:52:42 -0200

    Change makeItModularPrincipal.js

--- makeItModularPrincipal.js
+++ makeItModularPrincipal.js
@@ -1,6 +1,8 @@
 module = require('./makeItModular(Module).js');
+var dir = process.argv[2];
+var ext = process.argv[3];
 
-module(process.argv[2],process.argv[3],function(err, data) {
+module(dir, ext,function(err, data) {
   if (err) {
     console.log(err);
   }
