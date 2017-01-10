33caf801632d208785d4507a1aea6d795913d40d
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-01-10 15:09:32 -0200

    Module consign added

--- ntask-api/index.js
+++ ntask-api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import consign from "consign";
 const PORT = 3000;
+
 const app = express();
 
-app.get("/", (req, res) => res.json({status: "NTask API OK"}));
+app.set("json spaces", 4);
+app.set("port", 3000);
+
+consign()
+  .include("models");
+  .then("routes");
+  .into(app);
+
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
